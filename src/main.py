db5af9df5b56fa5bfcff2e9f7040cd621562c6cc
Loic Weiss <loic@mbp-de-loic.home>
2021-09-02 12:35:51 -0400

    [Init project] - Création Dockerfile comme ça 0 incompatibilité tu connais sisi les commits na3l sheitan

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import sys
+import bs4
+
+def main(args):
+    try:
+        print("dzqdqzufsejkzqbdjkdzbkdqzdzqdbqzbjqdkzbjkhfseuifui")
+        """bs4.BeautifulSoup("dq").find()"""
+    except:
+        print("error")
+        sys.exit(84)
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
