ebbcac7fee9366c6066ab9efd92d77c178a11a70
Loic Weiss <loic@MacBook-Pro-de-Loic.local>
2021-09-02 16:28:07 -0400

    [ADDING] - Getting all potions name

--- src/main.py
+++ src/main.py
@@ -1,10 +1,39 @@
 import sys
-import bs4
+from bs4 import BeautifulSoup
+import requests
+
+
+def check_upper_name(string):
+    last_char = string[-1]
+    if last_char.isupper():
+        string = string[:-1]
+        return check_upper_name(string)
+    return string
+
+
+class Potion:
+    def __init__(self, name):
+        self.name = name
+        self.url = name.replace(" ", "%20").replace("'", "%27")
 
 def main(args):
     try:
-        print("dzqdqzufsejkzqbdjkdzbkdqzdzqdbqzbjqdkzbjkhfseuifui")
-        """bs4.BeautifulSoup("dq").find()"""
+        page = requests.get("https://aonprd.com/Spells.aspx?Class=All")
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        for url in soup.find_all('td'):
+            tmp = url.text.replace("\n", "")
+            if tmp[0] == " ":
+                tmp = tmp.replace(" ", "", 1)
+            tmp = tmp.split(":")
+            tmp = tmp[0]
+            tmp = check_upper_name(tmp)
+            #print(tmp)
+
+            tmp_class = Potion(tmp)
+
+            print(tmp_class.url)
+
     except:
         print("error")
         sys.exit(84)
