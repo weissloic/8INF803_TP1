85dbaf438d09a63d4823e8fbab7ff42caf12c070
Hugo-87 <hugobourlion@gmail.com>
2021-09-21 09:38:12 -0400

    Préparation JSON

--- src/Exrercice 2/main.py
+++ src/Exrercice 2/main.py
@@ -1,5 +1,19 @@
+import pymongo
+import json
+
 def main():
-    print("Hello World!")
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["mydatabase"]
+    graph = {
+        "id": "Page_A",
+        "value": {
+            "pageRank": "1",
+            "adjlist": ["Page_B", "Page_C"]
+        }
+
+    }
+
+
 
 
 if __name__ == "__main__":
