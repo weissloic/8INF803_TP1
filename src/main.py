0cc8be7ec64d84eea1ba0ba7a92ef2ea133495e8
Loic Weiss <loic@MacBook-Pro-de-Loic.local>
2021-10-07 09:19:47 -0400

    lil refacto

--- src/main.py
+++ src/main.py
@@ -92,36 +92,76 @@ class MongoDB:
 
         reduce = Code("function (key, values) {"
                       "  var total = 0;"
-                      "  for (var i = 0; i < values.length; i++) {"
-                      "    total += values[i];"
-                      "  }"
-                      "  return total;"
                       "}")
 
-        result = self.collection.map_reduce(map, reduce, "myresults")
+        result = collection.map_reduce(map, reduce, "myresults")
         counter = 0
 
         for doc in result.find():
             counter += 1
             print(doc)
 
+        print(counter)
+
 class mySql():
     def __init__(self):
         self.conn = sqlite3.connect('example.db')
+
+
+def getComponent(find_infos):
+    print("Components: " + find_infos.group(4))
+
+    tmpComponent = find_infos.group(4)
+    if tmpComponent.find('('):
+        tmpComponent = re.sub(r"\([^()]*\)", "", find_infos.group(4))
+
+
+    print(tmpComponent.split(","))
+    listComp = tmpComponent.split(",")
+    listComp = [x.strip(' ') for x in listComp]
+    print(listComp)
+
+    return listComp
+
+def getResistance(find_infos, spell_found):
+    if spell_found:
+        print("Spells Resistance: " + find_infos.group(6))
+        if "yes" in find_infos.group(6):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def getLevel(classLinked, prs):
+    level = 0
+
+    if "wizard" in classLinked:
+        level = int(classLinked.split("wizard ")[1])
+    else:
+        spell_list = prs.extract_spell_list(classLinked)
+        level = prs.get_minimum_level(spell_list)
+
+    return level
+
+def getClassLinked(find_infos):
+
+    if find_infos.group(2).find('('):
+        tmp = re.sub(r"\([^()]*\)", "", find_infos.group(2))
+    else:
+        tmp = re.sub(r"\([^()]*\)", "", find_infos.group(2))
+
+    return tmp
 
 def main(args):
     # try:
 
     tt = Utils()
     prs = Parsing()
-    #client = MongoClient("mongodb://root:rootpassword@localhost:27017")
-    #db = client.business
-
     mongo = MongoDB()
-    #mongo.map_reduce_request()
+    mongo.map_reduce_request()
 
-
-    soup = prs.init_soup("Spells.aspx?Class=All")
+    """soup = prs.init_soup("Spells.aspx?Class=All")
 
     for url in soup.find_all('td'):
         spell_class = Spell(prs.get_spell_name(url))
@@ -133,7 +173,6 @@ def main(args):
 
         regex = spell_class.name + "<\/h1>.*(Level<\/b>(.*?))<.*Description<\/h3>"
         spellDiv = str(spellDiv).replace("\n", "")
-
         find_infos = re.search(regex, str(spellDiv))
 
         if find_infos:
@@ -142,45 +181,12 @@ def main(args):
 
             print("name class: " + spell_class.name)
             find_infos = re.search(regex, str(spellDiv))
-            spell_list = ""
             print(re.sub(r"\([^()]*\)", "", find_infos.group(2)))
 
-            tmp = find_infos.group(2)
-            if tmp.find('('):
-                tmp = re.sub(r"\([^()]*\)", "", find_infos.group(2))
-            else:
-                tmp = re.sub(r"\([^()]*\)", "", find_infos.group(2))
-
-            spell_list = prs.extract_spell_list(tmp)
-            #start = find_infos.group(2).find( '(' )
-
-            print("classe linked:", spell_list)
-
-            spell_class.level = prs.get_minimum_level(spell_list)
-
-            #print("Minimum Level: "+prs.get_minimum_level(spell_list))
-            print("Components: " + find_infos.group(4))
-
-            tmpComponent = find_infos.group(4)
-            if tmpComponent.find('('):
-                tmpComponent = re.sub(r"\([^()]*\)", "", find_infos.group(4))
-
-            print(tmpComponent.split(","))
-
-            spell_class.classLinked = tmp
-            spell_class.components = tmpComponent.split(",")
-
-
-            print("Spells Resistance: ")
-            if prs.spell_found:
-                print("Spells Resistance: " + find_infos.group(6))
-                if "yes" in find_infos.group(6):
-                    spell_class.resistance = True
-                else:
-                    spell_class.resistance = False
-            else:
-                spell_class.resistance = False
-
+            spell_class.classLinked = getClassLinked(find_infos)
+            spell_class.level = getLevel(spell_class.classLinked, prs)
+            spell_class.components = getComponent(find_infos)
+            spell_class.resistance = getResistance(find_infos, prs.spell_found)
 
             spell = {
                 'name': spell_class.name,
@@ -190,17 +196,14 @@ def main(args):
                 'spell_resistance': spell_class.resistance
             }
 
-            result = mongo.db.reviews.insert_one(spell)
+            #result = mongo.db.reviews.insert_one(spell)
         else:
             prs.counter_error += 1
 
 
         print("error: ", prs.counter_error)
         print("spell not displayed: ", prs.counter_spell_not_displayed)
-
-
-
-        print("---------------------------------\n")
+        print("---------------------------------\n")"""
 
 
 # except:
