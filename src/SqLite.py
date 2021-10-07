12219ceacfcf9077dc8c0e78570fe986ba370639
guidouche <72019765+guidouche@users.noreply.github.com>
2021-10-07 11:15:33 -0400

    Add select query and fix sqlite database creation

--- src/SqLite.py
+++ src/SqLite.py
@@ -5,11 +5,26 @@ from Spell import *
 db_name = 'SQLite.db'
 
 create_query = '''CREATE TABLE spell (
-                            name TEXT ,
+                            name TEXT PRIMARY KEY,
+                            level INTEGER,
                             components TEXT,
-                            spell_resistance  INTEGER );'''
+                            spell_resistance  INTEGER,
+                            class_linked TEXT);'''
+
+insert_query = ''' INSERT INTO spell(name,level,components,spell_resistance,class_linked)
+             VALUES(?,?,?,?,?) '''
 
 drop_table_query = '''DROP TABLE IF EXISTS spell'''
+
+select_spell_query='''select * from spell 
+WHERE 
+components = ' V'
+AND
+class_linked LIKE '%wizard 4%'
+				OR '%wizard 3%'
+				OR '%wizard 2%' 
+				OR '%wizard 1%' 
+'''
 
 
 class SqLite():
@@ -37,16 +52,30 @@ class SqLite():
             self.sqliteConnection.close()
 
     def put_spell(self,spell_class):
-        query = ''' INSERT INTO spell(name,components,spell_resistance)
-             VALUES(?,?,?) '''
+
         try:
             cursor = self.sqliteConnection.cursor()
-            cursor.execute(query,(spell_class.name,spell_class.components,spell_class.resistance,))
+            cursor.execute(insert_query,(spell_class.name,spell_class.level,':'.join(spell_class.components),spell_class.resistance,spell_class.classLinked,))
             self.sqliteConnection.commit()
             cursor.close()
             print("Data put done")
         except sqlite3.Error as error:
             print("can't put data", error)
+
+
+    def select_spell(self):
+        try:
+            cursor = self.sqliteConnection.cursor()
+            cursor.execute(select_spell_query)
+            self.sqliteConnection.commit()
+            result= cursor.fetchall()
+            cursor.close()
+            print("table creation done")
+            return result
+        except sqlite3.Error as error:
+            print("can't create SqliteTable", error)
+            self.sqliteConnection.close()
+
 
 
     def close_sql(self):
